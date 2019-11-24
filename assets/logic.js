7ac6d2d7f16e8de517ca56f9ba621c2aa80c5877
JGen1 <jengenes01@gmail.com>
2019-11-23 19:15:16 -0500

    Created landing page and basic layout.

---
+++ assets/logic.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+
+//     $("button").on("click",function(){
+//     var userSelection = $("input:text").val();
+//     console.log(userSelection);
+//     });
+
+
+
+
+
+// });
+
+
