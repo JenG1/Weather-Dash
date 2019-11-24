939ecd8f9f70f800ade9d3f44f74006f79b09d61
JGen1 <jengenes01@gmail.com>
2019-11-23 22:05:16 -0500

    Created search bar, added logic to add to add query to locations list.

--- assets/logic.js
+++ assets/logic.js
@@ -1,14 +1,10 @@
-// $(document).ready(function(){
-
-//     $("button").on("click",function(){
-//     var userSelection = $("input:text").val();
-//     console.log(userSelection);
-//     });
-
-
+$('#btn_createList').click(function(){
+    $('.ul_current').append($('<li>', {
+            text: $('#input_listName').val()
+    }));
+    document.getElementById("input_listName").value = "";
+});
 
 
-
-// });
 
 
