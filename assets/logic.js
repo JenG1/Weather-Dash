53158fb76ae39140667d6d6eacb00a420f1dc5f8
JGen1 <jengenes01@gmail.com>
2019-12-08 21:38:16 -0500

    Added UV index api call and retrieved UV data. Added classes to different ranges of UV index.

--- assets/logic.js
+++ assets/logic.js
@@ -1,91 +1,74 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // // function storeList() {
-    //     // Stringify and set "StoreList" key in localStorage to inputListNames array
-    //     localStorage.setItem("inputListNames", JSON.stringify(inputListNames));
-    // }
 
-    // $("#btn_createList").click(function(){
-    //     $.get("http://api.openweathermap.org/data/2.5/forecast?appid=fc321d547053f741cd8a4bb0b95c304a&q=Miami&count=10", function(data, status){
-    //       alert("Data: " + data + "\nStatus: " + status);
-    //     });
-    //   });
-    //Search for City and display weather plus 5 day forecast. Append list with new list item and clear search text field.
-    $('#btn_createList').on('click',function(){
-        event.preventDefault(); 
-        var listItem =  $('#input_listName').val();
-        console.log(listItem);
+  $('#btn_createList').on('click', function () {
+    event.preventDefault();
+    //Add city to list 
+    $('.ul_current').append($('<li>', {
+      text: $('#input_listName').val(),
+    }));
+    getCurrentWeather();
 
-        //Add city to list 
-        $('.ul_current').append($('<li>', {text: $('#input_listName').val(),
-        }));
-        ////Store City to local storage
-        //function storeList();
-        getCurrentWeather();
+  });
+
+  var APIKey = 'fc321d547053f741cd8a4bb0b95c304a';
+  //Ajax call to OpenWeather API
+  function getCurrentWeather() {
+    var citySearch = $('#input_listName').val();
+    var queryURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + '&units=imperial&appid=' + APIKey;
+
+    $.ajax({
+      url: queryURL1,
+      method: "GET"
+    }).then(function (response) {
+      getInfo(response);
     });
+  }
 
+  //Display information
+  function getInfo(response) {
 
-    var APIKey = 'fc321d547053f741cd8a4bb0b95c304a';
-    //Ajax call to OpenWeather API
-    function getCurrentWeather(){  
-      var citySearch =  $('#input_listName').val();
-      var queryURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&units=imperial&appid='+APIKey;
-      $.ajax({
-          url: queryURL1, 
-        // url: "http://api.openweathermap.org/data/2.5/forecast?appid=fc321d547053f741cd8a4bb0b95c304a&q="+"Baltimore"+"&count=1",
-          method: "GET"
-      }).then(function(response){
-          console.log(response);
-          getInfo(response);
-          getUv(response);
-        });
-    }
+    var cityName = response.city.name;
+    // var currentDate = response.list[0].dt_txt;
+    var rawDate = new Date((response.city.sunrise) * 1000);
+    var d = rawDate.getDate();
+    var m = rawDate.getMonth() + 1;
+    var y = rawDate.getFullYear();
+    var currentDate = (m + '/' + d + '/' + y);
+    cityName = $("#localCityName").text(cityName);
+    currentDate = $("#localCityDate").text(currentDate);
+    //Current Temperature
+    var temp = response.list[0].main.temp;
+    var humidity = response.list[0].main.humidity;
+    var windspd = response.list[0].wind.speed;
 
-    function getUV(response){
-      var lat = response.city.coord.lat;
-      console.log(lat);
-      console.log(long);
-      var long = response.city.coord.long;
-    }
-      
-      
-    //   )var uvURL = 'https://api.openweathermap.org/data/2.5/uvi?lat='37.75'&lon='-122.37'&appid='+APIKey;
-    // api.openweathermap.org/data/2.5/uvi/forecast?lat=37.75&lon=-122.37
-    //   $.ajax({
-    //   url: uvUrl,
-    //   method: 'GET'
-    // })
-    // .then(function(uvData) {
-
-    //   var uvIndex = uvData.value;
-    //   var uv = $('<p>');
-    //   uv.html('UV Index: <span id="uvSpan">' + uvIndex + '</span>');
-    //   console.log(uvIndex);    
-
-
-    //Display information
-    function getInfo(response){
-
-      var cityName = response.city.name;
-      // var currentDate = response.list[0].dt_txt;
-      var rawDate = new Date((response.city.sunrise) * 1000);
-      var d = rawDate.getDate();
-      var m = rawDate.getMonth() + 1;
-      var y = rawDate.getFullYear();
-      var currentDate = (m + '/' + d + '/' + y);
-      cityName = $("#localCityName").text(cityName);
-      currentDate = $("#localCityDate").text(currentDate);
-      //Current Temperature
-      var temp = response.list[0].main.temp;
-      var humidity = response.list[0].main.humidity;
-      var windspd = response.list[0].wind.speed;
-
-      temp = $('#temp').text('Temperature: '+ temp +'°F');
-      humidity = $('#humidity').text('Humidity: '+ humidity +'%');
-      windspd = $('#windspd').text('Wind Speed: '+ windspd + 'MPH');
-    }
-
+    temp = $('#temp').text('Temperature: ' + temp + '°F');
+    humidity = $('#humidity').text('Humidity: ' + humidity + '%');
+    windspd = $('#windspd').text('Wind Speed: ' + windspd + 'MPH');
+    // Coordinates for UV API call
+    var lat = response.city.coord.lat;
+    var lon = response.city.coord.lon;
+    var uvUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon;
+    // UV
+    $.ajax({
+      url: uvUrl,
+      method: 'GET'
+      })
+      .then(function(uvData) {
+      var uvIndex = uvData.value;
+      var uv = $('#uv');
+      uv.html(" "+uvIndex);
+      if (uvIndex >= 0 && uvIndex <= 3) {
+          $(uv).addClass('lowUV');
+      } else if (uvIndex > 3 && uvIndex <= 6) {
+          $(uv).addClass('medUV');
+      } else if (uvIndex > 6) {
+          $(uv).addClass('highUV')
+      }
+    })
+  }
+  
 });
 
 
