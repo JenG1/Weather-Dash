cb81a3c531a696b5322ac9ac6d6e99999a14a634
JGen1 <jengenes01@gmail.com>
2020-03-16 23:54:37 -0400

    ...

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-    let city = $('#cityDiv');
-    let searchF = $('#searchForm');
-    let searchB = $('#searchBar');
-    let historyEl = $('#history');
-    let firstCard = $('#card1');S
-    let secondCard = $('#card2');
-    let thirdCard = $('#card3');
-    let fourthCard = $('#card4');
-    let fifthCard = $('#card5');
-    let citiesList = [];
-    let cityQ = '';
-    let btn = $('#btnn');
-    let APIKey = 'fc321d547053f741cd8a4bb0b95c304a'; 
+    var cityEl = $('#cityDiv');
+    var searchF = $('#searchForm');
+    var searchB = $('#searchBar');
+    var historyEl = $('#history');
+    var firstCard = $('#card1');
+    var secondCard = $('#card2');
+    var thirdCard = $('#card3');
+    var fourthCard = $('#card4');
+    var fifthCard = $('#card5');
+    var citiesList = [];
+
+    var cityQ = '';
+    var btn = $('#btnn');
+    var APIKey = 'fc321d547053f741cd8a4bb0b95c304a'; 
+
 
     function clearDivs() {
         firstCard.empty();
@@ -23,7 +25,7 @@ $(document).ready(function() {
 
     function clearHistory() {
         historyEl.empty();
-        city.empty();
+        cityEl.empty();
         firstCard.empty();
         secondCard.empty();
         thirdCard.empty();
@@ -37,7 +39,7 @@ $(document).ready(function() {
     }
 
     function currentWeather() {
-        let queryURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityQ + '&units=imperial&appid=' + APIKey;
+        var queryURL1 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityQ + '&units=imperial&appid=' + APIKey;
         // Weather API
         $.ajax({
                 url: queryURL1,
@@ -45,57 +47,57 @@ $(document).ready(function() {
             })
             .then(function(data) {
 
-                city.empty();
+                cityEl.empty();
 
-                let city = $('<div>');
-                let rawDate = new Date((data.city.sunrise) * 1000);
-                let d = rawDate.getDate();
-                let m = rawDate.getMonth() + 1;
-                let y = rawDate.getFullYear();
-                let date = (m + '/' + d + '/' + y);
+                var city = $('<div>');
+                var rawDate = new Date((data.city.sunrise) * 1000);
+                var d = rawDate.getDate();
+                var m = rawDate.getMonth() + 1;
+                var y = rawDate.getFullYear();
+                var date = (m + '/' + d + '/' + y);
 
                 city.html('<h1 id="cityName"> ' + data.city.name + ' ' + date + '</h1>');
-                city.append(city);
+                cityEl.append(city);
 
-                let lat = data.city.coord.lat;
-                let lon = data.city.coord.lon;
-                let uvUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon;
+                var lat = data.city.coord.lat;
+                var lon = data.city.coord.lon;
+                var uvUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon;
 
-                let temp = $('<div>');
+                var temp = $('<div>');
                 temp.html('<p>Current Temperature: ' + data.list[0].main.temp + '&#8457</p>');
                 temp.addClass('cityPg');
-                city.append(temp);
+                cityEl.append(temp);
 
-                let humidity = $('<p>').text('Humidity: ' + data.list[0].main.humidity + '%');
+                var humidity = $('<p>').text('Humidity: ' + data.list[0].main.humidity + '%');
                 humidity.addClass('cityPg');
-                city.append(humidity);
+                cityEl.append(humidity);
 
-                let wind = $('<p>').text('Wind Speed: ' + data.list[0].wind.speed + ' MPH');
+                var wind = $('<p>').text('Wind Speed: ' + data.list[0].wind.speed + ' MPH');
                 wind.addClass('cityPg');
-                city.append(wind);
-                let counter = 1;
+                cityEl.append(wind);
+                var counter = 1;
 
-                for (let i = 3; i < data.list.length; i += 8) {
+                for (var i = 3; i < data.list.length; i += 8) {
 
-                    let weatherCard = $("div[data-count='" + counter + "']");
-                    let rawDate = new Date((data.list[i].dt) * 1000);
-                    let d = rawDate.getDate();
-                    let m = rawDate.getMonth() + 1;
-                    let y = rawDate.getFullYear();
-                    let date = (m + '/' + d + '/' + y);
-                    let dateEl = $('<h5>');
+                    var weatherCard = $("div[data-count='" + counter + "']");
+                    var rawDate = new Date((data.list[i].dt) * 1000);
+                    var d = rawDate.getDate();
+                    var m = rawDate.getMonth() + 1;
+                    var y = rawDate.getFullYear();
+                    var date = (m + '/' + d + '/' + y);
+                    var dateEl = $('<h5>');
                     dateEl.text(date);
                     weatherCard.append(dateEl);
-                    let weatherBlockTemp = $('<div>');
+                    var weatherBlockTemp = $('<div>');
                     weatherBlockTemp.html('<p>' + data.list[i].main.temp + '&#8457</p>');
                     weatherCard.append(weatherBlockTemp);
-                    let weatherIcon = $('<img>');
+                    var weatherIcon = $('<img>');
                     iconSrc = 'http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png';
                     weatherIcon.attr('src', iconSrc);
                     weatherIcon.attr('width', '60px');
                     weatherIcon.attr('height', '60px')
                     weatherCard.append(weatherIcon);
-                    let humidity2 = $('<p>').text('Humidity: ' + data.list[i].main.humidity + ' %');
+                    var humidity2 = $('<p>').text('Humidity: ' + data.list[i].main.humidity + ' %');
                     weatherCard.append(humidity2);
                     counter += 1;
                 }
@@ -106,9 +108,10 @@ $(document).ready(function() {
                     })
                     .then(function(uvData) {
 
-                        let uvIndex = uvData.value;
-                        let uv = $('<p>');
+                        var uvIndex = uvData.value;
+                        var uv = $('<p>');
                         uv.html('UV Index: <span id="uvSpan">' + uvIndex + '</span>');
+
 
                         if (uvIndex >= 0 && uvIndex <= 3) {
                             $(uv).addClass('lowUV');
@@ -117,10 +120,12 @@ $(document).ready(function() {
                         } else if (uvIndex > 6) {
                             $(uv).addClass('highUV')
                         }
-                        city.append(uv);
+                        cityEl.append(uv);
                     })
 
             })
+
+
     }
 
     function search() {
@@ -138,17 +143,23 @@ $(document).ready(function() {
     function renderWeather() {
         historyLink = $('<div>');
         historyText = '<p onclick="currentWeather"> ' + cityQ + '</p>';
+        // Save city name to storage
         citiesList.push(historyText);
         console.log(historyText);
+
         localStorage.setItem('cities', JSON.stringify(historyText));
         historyLink.append(historyText);
         historyLink.attr('data-city', cityQ);
         historyLink.addClass('historyBtn');
         historyEl.append(historyLink);
     }
+
     searchF.on('submit', search);
+
     btn.on('click', function() {
         clearHistory();
+
+
     });
 });
 
